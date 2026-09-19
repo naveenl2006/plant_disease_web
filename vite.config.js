db242d1b3e5b2b6bb8f2a19b03705692086a9429
naveenl2006 <naveenlakshmanan.c@gmail.com>
2026-09-20 00:41:45 +0530

    new updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/hf": {
-        target: "https://naveen2916-plantdisease.hf.space",
+      '/hf': {
+        target: 'https://naveen2916-plantdisease.hf.space',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/hf/, ""),
+        rewrite: (path) => path.replace(/^\/hf/, ''),
       },
     },
   },
-});
+})
